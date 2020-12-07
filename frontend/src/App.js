d846b6cdd63b100e640d05dd76f0fd2171790d47
Patryk Gajek <pgajek94@gmail.com>
2020-12-07 13:54:43 +0100

    initial commit with create-react-app structure and firebase config

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>MERN WHATS UP CLONE</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC8UjhumxTXhcx8ZDkS6uthlAQamlI4Mxw",
+  authDomain: "whats-up-clone-c2409.firebaseapp.com",
+  projectId: "whats-up-clone-c2409",
+  storageBucket: "whats-up-clone-c2409.appspot.com",
+  messagingSenderId: "124166287683",
+  appId: "1:124166287683:web:a51aa80c9e404e0f503ff6",
+};

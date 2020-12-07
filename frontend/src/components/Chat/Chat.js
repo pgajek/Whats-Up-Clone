4d0e2aea18007017cf39e7297aff1418a89f2ae3
Patryk Gajek <pgajek94@gmail.com>
2020-12-07 16:00:02 +0100

    components styled and wrapped

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,8 +1,31 @@
 import React from "react";
-import "Chat.css";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
 
+import { MoreVert, AttachFile, SearchOutlined } from "@material-ui/icons";
 const Chat = () => {
-  return <div className="caht"> Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+      </div>
+      <div className="chat__headerInfo">
+        <h3>room name</h3>
+        <p>last seen at...</p>
+      </div>
+      <div className="chat__headerRight">
+        <IconButton>
+          <SearchOutlined />
+        </IconButton>
+        <IconButton>
+          <AttachFile />
+        </IconButton>
+        <IconButton>
+          <MoreVert />
+        </IconButton>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,8 +1,42 @@
 import React from "react";
 import "./Sidebar.css";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import { Avatar, IconButton } from "@material-ui/core";
+import SidebarChat from "../SidebarChat/SidebarChat";
 
 const Sidebar = () => {
-  return <div className="sidebar"> Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://fwcdn.pl/fpo/91/13/299113/7332755.3.jpg" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;

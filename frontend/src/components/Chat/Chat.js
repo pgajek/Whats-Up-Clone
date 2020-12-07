4828ad6072ca78ce741a83c7cd7649e34323fdaa
Patryk Gajek <pgajek94@gmail.com>
2020-12-07 16:33:45 +0100

    frontend ui finished

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -2,26 +2,62 @@ import React from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 
-import { MoreVert, AttachFile, SearchOutlined } from "@material-ui/icons";
+import {
+  MoreVert,
+  AttachFile,
+  SearchOutlined,
+  InsertEmoticon,
+  Mic,
+} from "@material-ui/icons";
 const Chat = () => {
   return (
     <div className="chat">
       <div className="chat__header">
         <Avatar />
+        <div className="chat__headerInfo">
+          <h3>room name</h3>
+          <p>last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
       </div>
-      <div className="chat__headerInfo">
-        <h3>room name</h3>
-        <p>last seen at...</p>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Sonny</span>
+          this is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Sonny</span>
+          this is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Sonny</span>
+          this is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
       </div>
-      <div className="chat__headerRight">
+      <div className="chat__footer">
         <IconButton>
-          <SearchOutlined />
+          <InsertEmoticon />
         </IconButton>
+        <form>
+          <input type="text" placeholder="type a message" />
+          <button type="submit">Send a message</button>
+        </form>
         <IconButton>
-          <AttachFile />
-        </IconButton>
-        <IconButton>
-          <MoreVert />
+          <Mic />
         </IconButton>
       </div>
     </div>
